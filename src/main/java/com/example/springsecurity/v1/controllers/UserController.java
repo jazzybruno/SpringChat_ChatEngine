8ae50079d95119054c9f49a8b950c7a26b2f053e
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-04-01 23:21:33 +0200

    Added the necessary dependencies

---
+++ src/main/java/com/example/springsecurity/v1/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.example.springsecurity.v1.controllers;
+
+public class UserController {
+    private static String CHAT_ENGINE_PROJECT_ID = "925924c7-aaff-4212-b293-b7e06356c7a9";
+    private static String CHAT_ENGINE_PRIVATE_KEY = "6127f895-5225-4b8f-a15b-9bf3b0135858";
+
+    // the login method
+
+}
